8df4518d189bbfdd1488aafd57f46092fbc9ef5a
skylabo <157952058+skylabo@users.noreply.github.com>
2024-02-15 07:50:10 +0000

    pugを反映させた

--- first_app/app.js
+++ first_app/app.js
@@ -8,8 +8,14 @@ const bodyparser = require('body-parser');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true}));
 
+// pugテンプレートを使用
+// const pug = require('pug');
+app.set('view engine' , 'pug');
+app.set('views' , './views');
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/' + TEMPLATE_DIR + '/' + 'template.html');
+    // res.sendFile(__dirname + '/' + TEMPLATE_DIR + '/' + 'template.html');
+    res.render('form');
 });
 
 app.listen(port,()=>{
@@ -17,5 +23,6 @@ app.listen(port,()=>{
 });
 
 app.post('/input', (req,res)=>{
-    console.log('入力文字列=' + req.body.sample_text);    
+    console.log('入力文字列=' + req.body.sample_text);
+    res.render('show_input' , {'input_text' : req.body.sample_text});    
 });
