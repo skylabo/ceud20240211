055a58690782a24af2effd5c0765b3e3d6d020e9
skylabo <157952058+skylabo@users.noreply.github.com>
2024-02-17 11:36:20 +0000

    sqlが操作できるようになった

--- crud/app.js
+++ crud/app.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -14,3 +14,21 @@ connection.connect((err) => {
     }
     console.log('Connected to MySQL!ついにMYSQLに接続できましたね。');
 });
+
+// SQL文を書いて実行
+const sql = 'SELECT * FROM animal';
+// const sql ='UPDATE animal SET name="トカゲ２" WHERE id=1';
+connection.query(sql, (err, result) => {
+    if (err) {
+        console.error(`Error!: ${sql}`);
+        console.error(err.sqlMessage);
+        return;
+    }
+    // 結果表示
+    console.log('id, name, size, memo');
+    for (let i = 0; i < result.length; i++) {
+        console.log(`${result[i].id}, ${result[i].name}, ${result[i].size}, ${result[i].memo}`);
+    }
+});
+
+connection.end();
